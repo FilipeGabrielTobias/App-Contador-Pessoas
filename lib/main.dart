94881b3a38a3d2e0cd7ad65ca30333c9b7ef698f
Filipe Tobias <filipe.tobias@senior.com.br>
2022-02-28 10:05:30 -0300

    Reajustes no código fonte e troca de icone e label do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,132 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessoas",
-      home: Home()));
+  runApp(
+    const MyApp(),
+  );
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-
-  int _people = 0;
-  String _infoText = "Pode Entrar!";
-
-  void _changePeople(int delta) {
-    setState(() {
-      _people += delta;
-
-      if(_people < 0) {
-        _infoText = "Mundo Invertido?!";
-      } else if(_people <= 10) {
-        _infoText = "Pode Entrar!";
-      } else {
-        _infoText = "Lotado!";
-      }
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          "images/restaurant.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
+    return MaterialApp(
+      title: "Contador de Pessoas", 
+      home: HomePage(), 
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _count = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _decrement() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  void _increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  bool get isEmpty => _count == 0;
+  bool get isFull => _count == 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container (
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/restaurant.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-        Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Pessoas: $_people",
-              style:
-              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text (
+                isFull ? 'Lotado!' : 'Pode Entrar!',
+                style: TextStyle(
+                  color: isFull ? Colors.red : Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30.0
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "$_count",
+                style: TextStyle(
+                  color: isFull ? Colors.red : Colors.white, 
+                  fontSize: 100,
+                ),
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                      onPressed: () {
-                        _changePeople(1);
-                      },
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      )),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    onPressed: isEmpty ? null : _decrement,
+                    child: const Text(
+                      "Saiu",
+                      style: const TextStyle(
+                        fontSize: 16.0, 
+                        color: Colors.black
+                      ),
+                    )
+                  ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                      onPressed: () {
-                        _changePeople(-1);
-                      },
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      )),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    onPressed: isFull ? null : _increment,
+                    child: const Text(
+                      "Entrou",
+                      style: const TextStyle(
+                        fontSize: 16.0, 
+                        color: Colors.black
+                      ),
+                    )
+                  ),
                 ),
               ],
             ),
-            Text(
-              "$_infoText",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0),
-            )
           ],
         )
-      ],
+      ),
     );
   }
 }
-
